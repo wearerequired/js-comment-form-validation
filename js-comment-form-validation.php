9b37f61d1725b96079347a7363c771e9d7c1e7a1
Pascal Birchler <hello@pascalbirchler.ch>
2017-05-08 18:43:36 +0200

    Add `Domain Path` to plugin header

--- js-comment-form-validation.php
+++ js-comment-form-validation.php
@@ -9,6 +9,7 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: js-comment-form-validation
+ * Domain Path: /languages
  *
  * Copyright (c) 2017 required (email: info@required.ch)
  *
